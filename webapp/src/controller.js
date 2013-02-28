ca3bd52fafcb883b14ecdd7c301b56b9f4183db0
Paolo Burelli <burelli@gmail.com>
2013-02-28 21:53:48 +0100

    Loading fixed on other devices

--- webapp/src/controller.js
+++ webapp/src/controller.js
@@ -10,65 +10,79 @@ function delayedSearch(value) {
 	}, 1000);
 }
 
+var started = false;
+function start() {
+	if (!started) {
+
+		sounDojo = new SounDojo();
+
+		sounDojo.onPlay = function() {
+			if (document.getElementById('play') != null)
+				document.getElementById('play').id = 'pause';
+		}
+
+		sounDojo.onPause = function() {
+			if (document.getElementById('pause') != null)
+				document.getElementById('pause').id = 'play';
+		}
+
+		sounDojo.onVideoLoaded = function() {
+			if (followCurrentTrack)
+				showArtistPage(sounDojo.trackList[sounDojo.currentTrackIndex].artist);
+			updateTrackList();
+		}
+
+		sounDojo.onSearchStarted = function() {
+			showSearchThrobber();
+		}
+
+		sounDojo.onSearchCompleted = function() {
+			showResults();
+			hideSearchThrobber();
+		}
+
+		sounDojo.onTracklistLoadingStarted = function() {
+			startWaiting();
+		}
+
+		sounDojo.onTracklistLoadingCompleted = function(param) {
+			if (param === undefined)
+				refillTrackList();
+			else
+				showLoadingError(param);
+
+			stopWaiting();
+		}
+
+		sounDojo.onLastFmLogin = function() {
+			showLastfmLoginState();
+			setTimeout(loadHome, 100);
+		}
+
+		sounDojo.onLastFmLogout = function() {
+			showLastfmLoginState();
+			setTimeout(loadHome, 100);
+		}
+
+		sounDojo.onTrackLoved = function(track, artist) {
+			showInfo("You loved " + track, "img/controls-love.png");
+		}
+
+		sounDojo.onTrackBanned = function(track, artist) {
+			showInfo("You banned " + track, "img/controls-ban.png");
+		}
+
+		sounDojo.init();
+		initUI();
+		started = true;
+	}
+}
+
+
 $(document).ready(function() {
-	sounDojo = new SounDojo();
-
-	sounDojo.onPlay = function() {
-		if (document.getElementById('play') != null)
-			document.getElementById('play').id = 'pause';
-	}
-
-	sounDojo.onPause = function() {
-		if (document.getElementById('pause') != null)
-			document.getElementById('pause').id = 'play';
-	}
-
-	sounDojo.onVideoLoaded = function() {
-		if (followCurrentTrack)
-			showArtistPage(sounDojo.trackList[sounDojo.currentTrackIndex].artist);
-		updateTrackList();
-	}
-
-	sounDojo.onSearchStarted = function() {
-		showSearchThrobber();
-	}
-
-	sounDojo.onSearchCompleted = function() {
-		showResults();
-		hideSearchThrobber();
-	}
-
-	sounDojo.onTracklistLoadingStarted = function() {
-		startWaiting();
-	}
-
-	sounDojo.onTracklistLoadingCompleted = function(param) {
-		if (param === undefined)
-			refillTrackList();
-		else
-			showLoadingError(param);
-
-		stopWaiting();
-	}
-
-	sounDojo.onLastFmLogin = function() {
-		showLastfmLoginState();
-		setTimeout(loadHome, 100);
-	}
-
-	sounDojo.onLastFmLogout = function() {
-		showLastfmLoginState();
-		setTimeout(loadHome, 100);
-	}
-
-	sounDojo.onTrackLoved = function(track, artist) {
-		showInfo("You loved " + track, "img/controls-love.png");
-	}
-
-	sounDojo.onTrackBanned = function(track, artist) {
-		showInfo("You banned " + track, "img/controls-ban.png");
-	}
-	
-	sounDojo.init();
-	initUI();
+	setTimeout(start,2000);
 });
+
+function onYouTubeIframeAPIReady() {
+	start();
+}
